82935802c4888439da2f28070a3ff481e6c1a9ae
xiz <shizuka.kakimoto@linkage.jp.net>
2015-11-28 13:55:32 +0900

    Finish login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
