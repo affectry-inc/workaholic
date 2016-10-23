c8b0e1717e58afbb9741e923e7c7a343e457fd85
xiz <shizuka.kakimoto@linkage.jp.net>
2016-10-24 00:38:30 +0900

    redirect 404 on unauthorized access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,31 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  private
-    
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-	redirect_to login_url
-      end
+  def render_404(e = nil)
+    logger.info "Rendering 404 with exception: #{e.message}" if e
+
+    #binding.pry
+    if request.xhr?
+      render json: { error: '404 error' }, status: 404
+    else
+      format = params[:format] == :json ? :json : :html
+      render file: Rails.root.join('public/404.html'),
+        status: 404, layout: false, content_type: 'text/html'
     end
+  end
+
+  def require_admin
+    render_404 unless logged_in? && current_user.is_admin
+  end
+
+  private
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+    redirect_to login_url
+    end
+  end
 end
